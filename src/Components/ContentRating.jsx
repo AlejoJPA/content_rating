15b346eef519d4ee0be893057bac29d05fa7fef3
Alejo <alejojpa2@gmail.com>
2024-10-11 10:06:04 +0200

    second commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -4,12 +4,16 @@ import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
-    super();
+    super(); // JS function: it allows to access the properties and methods of the parent class within the child class
+    this.state = {likes: 0, dislikes: 0}; // Setthng Initial state
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating (Alejo tested)</h1>
+      <h1>Text Content Rating (Alejo tested)</h1>
+      <div classNamme = 'content-rating'/>
+        <p> //Add text here</p>
+      </div>
      </>
     );
   }
